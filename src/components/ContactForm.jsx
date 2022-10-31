3c88eae26baa8bceaa9d67c55e3c8409eadef5dc
lukecode <lucassocorrosilva@gmail.com>
2022-10-31 15:29:12 -0300

    Correções

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -8,7 +8,7 @@ const ContactForm = () => {
             <input type="text" placeholder='Nome' />
             <input type="text" placeholder='Sobrenome' />
             <input type="text" placeholder='Email' />
-            <textarea type="text" placeholder='Menssagem' rows="4"></textarea>
+            <textarea type="text" placeholder='Mensagem' rows="4"></textarea>
             <button>Enviar</button>
         </form>
     </div>
